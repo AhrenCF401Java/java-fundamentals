55dc3cec49dddf55a6a6fca9fe367c8d0bd1a5ef
ahren <ahrenswett@gmail.com>
2019-09-18 13:52:40 -0700

    All working

---
+++ basicLibrary/README.md
@@ -0,0 +1,11 @@
+# Lab 02: Arrays, for loops, imports, ArrayLists
+##Work done today
+* made a method that rolls a 6 sided dice and creates 
+a random array that length is decided by a user input.
+
+* wrote a test that test to see if the array is the correct length
+
+* wrote a method that checks for duplicates in an array
+
+* wrote a test to make sure an array that has duplicates returns true
+
--- basicLibrary/src/main/java/basicLibrary/Library.java
+++ basicLibrary/src/main/java/basicLibrary/Library.java
@@ -3,8 +3,39 @@
  */
 package basicLibrary;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Library {
     public boolean someLibraryMethod() {
         return true;
     }
+
+
+    public static int[] roll(int rolls) {
+        Random rnd = new Random();
+        int[] numsRolled = new int[rolls];
+        System.out.println(numsRolled + " Arr");
+        for (int i = 0; i < numsRolled.length; i++) {
+            numsRolled[i] = rnd.nextInt((6 - 1) + 1) + 1;
+            System.out.println(i);
+        }
+
+        return numsRolled;
+    }
+
+    public static boolean containsDuplicates(int[] arr){
+        System.out.println(Arrays.toString(arr));
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]==arr[j]){
+                    return true;
+
+                }
+            }
+        }
+        return false;
+    }
+
 }
